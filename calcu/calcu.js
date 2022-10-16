2c9daae723cd7a4a1f20e9a4932cc7b941402b26
SIVASANGARANKK <sivasucbe@gmail.com>
2022-10-16 10:18:31 +0530

    qca

---
+++ calcu/calcu.js
@@ -0,0 +1,22 @@
+//Function To Display Values On screen
+function dis(value) {
+    document.getElementById("screen").value += value;
+}
+
+// Function For Evaluation
+function solve() {
+    let x = document.getElementById("screen").value;
+    let y = eval(x);
+    document.getElementById("screen").value = y;
+}
+
+//Function For clearing the screen
+function clr() {
+    document.getElementById("screen").value = ""
+}
+
+//backspacing
+function backspace() {
+    let screen = document.getElementById("screen").value;
+    document.getElementById("screen").value = screen.substring(0, screen.length - 1);
+}
